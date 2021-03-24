1afc8e60979fa5974511646810f2e4a5dd9fdc9a
Ankit Yadav <you@example.com>
2021-03-24 15:03:40 +0530

    Updates

---
+++ precache-manifest.0face8886a3888dd69bbdc30a190a8e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7ebbe5204668f268301c64da344c76",
+    "url": "/Covid-19_LiveTracker/index.html"
+  },
+  {
+    "revision": "fd0dd8bad1772233801d",
+    "url": "/Covid-19_LiveTracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "38cb637b4df057ce7aa4",
+    "url": "/Covid-19_LiveTracker/static/css/main.1e51d60f.chunk.css"
+  },
+  {
+    "revision": "fd0dd8bad1772233801d",
+    "url": "/Covid-19_LiveTracker/static/js/2.7d1b0a0d.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-19_LiveTracker/static/js/2.7d1b0a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38cb637b4df057ce7aa4",
+    "url": "/Covid-19_LiveTracker/static/js/main.76642463.chunk.js"
+  },
+  {
+    "revision": "2e893af8407abf59ddec",
+    "url": "/Covid-19_LiveTracker/static/js/runtime-main.c510569b.js"
+  }
+]);
