0c8feebfc5eddba3d0cf70bc7a455d8d1431aa59
Ankit Yadav <you@example.com>
2021-03-24 14:41:21 +0530

    c

---
+++ build/precache-manifest.7625bdf817e2a463b930bf5d0ea10d0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc235dc52881d4a8993ada5ea2491344",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "1d810367e6bfc9430eb3",
+    "url": "/cv/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "3e12729723b21df5e322",
+    "url": "/cv/static/css/main.1e51d60f.chunk.css"
+  },
+  {
+    "revision": "1d810367e6bfc9430eb3",
+    "url": "/cv/static/js/2.f4cba2e5.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/cv/static/js/2.f4cba2e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e12729723b21df5e322",
+    "url": "/cv/static/js/main.cfca8951.chunk.js"
+  },
+  {
+    "revision": "2cb5c8df3634246629ba",
+    "url": "/cv/static/js/runtime-main.3526c085.js"
+  }
+]);
